b8fd5605f77db4215653e73acd722cde52f4ce29
obrdanie <72950043+obrdanie@users.noreply.github.com>
2024-03-07 13:22:28 -0500

    Changed main again again again

--- Time/src/main/java/org/example/Time.java
+++ Time/src/main/java/org/example/Time.java
@@ -7,45 +7,22 @@ public class Time
 {
     public static void main(String[] args)
     {
-//        try
-//        {
-//
-//            String time = JOptionPane.showInputDialog(null,
-//                    "Enter a time in the format hh:mm:ss", "Enter Time",
-//                    JOptionPane.QUESTION_MESSAGE);
-//
-//            int totalSeconds = getTotalSeconds(time);
-//            JOptionPane.showMessageDialog(null, totalSeconds, "Total Seconds",
-//                    JOptionPane.INFORMATION_MESSAGE);
-//        }
-//        catch(StringIndexOutOfBoundsException e)
-//        {
-//            JOptionPane.showMessageDialog(null,
-//                    "You entered the time in the wrong format.\n" +
-//                            "Please enter the time in the form hh:mm:ss",
-//                    "Invalid Time", JOptionPane.ERROR_MESSAGE);
-//        }
-//        catch(NumberFormatException e)
-//        {
-//            JOptionPane.showMessageDialog(null,
-//                    "You entered an invalid time.\nPlease enter numbers only.",
-//                    "Invalid Time",	JOptionPane.ERROR_MESSAGE);
-//        }
-//        catch(Exception e)
-//        {
-//            System.out.println("An unexpected Exception occurred");
-//        }
-
-        int totalSeconds = getTotalSeconds("12:05:05:999");
+        int totalSeconds = getTotalSeconds("10:10:10");
         System.out.println("**GH Total Seconds = "+totalSeconds);
+
     }
 
-    public static int getTotalSeconds(String time) throws NumberFormatException, StringIndexOutOfBoundsException {
+    public static int getTotalSeconds(String time)throws NumberFormatException, StringIndexOutOfBoundsException {
+
+        System.out.println("get total seconds 1");
         int hours = getTotalHours(time);
-        int minutes = getTotalMinutes(time);
+        System.out.println("get total seconds 2");
+        //we will eventually multiply the hours by 3600 + the minutes by 60 + the seconds
+        int minutes = getTotalMinutes (time);
+        System.out.println("get total seconds 3");
         int seconds = getSeconds(time);
-        int milliseconds = getMilliseconds(time);
-        return hours * 3600 + minutes * 60 + seconds + milliseconds / 1000;
+        System.out.println("get total seconds 4");
+        return hours * 3600 + minutes * 60 + seconds;
     }
 
     public static int getSeconds(String time) throws NumberFormatException, StringIndexOutOfBoundsException
@@ -56,7 +33,7 @@ public class Time
 
     public static int getTotalMinutes(String time) throws NumberFormatException, StringIndexOutOfBoundsException
     {
-        if (time.length()>8)
+        if (time.length()>11)
         {
             throw new NumberFormatException("your time was too long!");
         }
@@ -68,14 +45,9 @@ public class Time
         return Integer.parseInt(time.substring(0,2));
     }
 
-    public static int getMilliseconds(String time) throws NumberFormatException, StringIndexOutOfBoundsException {
-
-        if (time.length() < 12) {
-            throw new StringIndexOutOfBoundsException("Your time input is too short to include milliseconds!");
-        }
-        return Integer.parseInt(time.substring(9, 12));
+    public static int getMilliseconds(String time)throws NumberFormatException, StringIndexOutOfBoundsException {
+        return Integer.parseInt(time.substring(9,11));
     }
-
 }
 
 
